1cd4179a9e5b61d2839d2b3142872510b6552236
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-05 04:58:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/create_puppy' do 
+    erb :create_puppy
+  end
+
   post '/create_puppy' do
     @puppy = Puppy.new(params[name], params[breed], params[age])
     erb :display_puppy
